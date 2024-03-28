13a2d806a44c32154bb1fcd2fa494f5e8cff846b
Raller <ms.mashanov@mail.ru>
2024-03-28 14:59:00 +0300

    Testng Test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCashbackHack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void shouldNotCashbackHack() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        Assert.assertEquals(actual, expected);
+    }
+}
