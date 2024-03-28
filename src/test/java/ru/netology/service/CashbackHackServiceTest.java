57206bce479cd394043735dfd7c62d7720f19f2c
Raller <ms.mashanov@mail.ru>
2024-03-28 15:05:48 +0300

    JUnit4 Test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,6 +1,6 @@
 package ru.netology.service;
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
     @Test
